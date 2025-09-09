d713cd0dda21954a8ddc238a5da9c2f3747442bf
Yasmin Angel <yasminangel2026@gmail.com>
2025-09-09 16:42:25 -0300

    Add files via upload

---
+++ Meu Primeiro Código.py
@@ -0,0 +1,4 @@
+nome = input ("Qual é o seu nome?")
+print(nome)
+idade = input ("Qual é a sua idade?")
+música = input ("Qual é a sua música preferida?")
---
+++ Untitled-2.py
@@ -0,0 +1,73 @@
+#print("Yasmin" * 80000)
+#exemplo de váriavel váriavel do tipo texto
+fruta = "manga"
+print(type(fruta))
+#Exemplo Váriavel do tipo INT é inteiro, números inteiro 
+idade = int(17)
+# Exemplo de número quebrado, float
+nota = float(9.5)
+
+# booleana 
+vc_curte = False
+
+###EXERCICIO 1
+
+# criar uma váriavel que peça o nome de usuário.
+# criar uma váriavel do tipo float, que peça a nota do usuário.
+# criar uma váriavel que peça a idade do usuário.
+# depois de um print nas suas váriaveis.
+
+
+nome = input("Digite o seu nome: ")
+print(nome)
+nota = float(input("Digite a sua nota:"))
+print(nota)
+idade = int(input("Digite a sua idade:"))
+print(idade)
+
+###EXERCICIO 2
+
+# crie uma váriavel que se chama nome_carro, coloque dentro dessa váriavel o nome do carro dos seus sonhos.
+# crie uma váriavel que se chama preço,coloque dentro dessa váriavel o preço do carro.
+# crie uma váriavel que se chama ano,coloque dentro dessa váriavel o ano do carro.
+
+nome_carro = "corvette v8"
+print(nome_carro)
+preco = float(1.0000)
+print (preco)
+ano = int(2008)
+print(ano)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
